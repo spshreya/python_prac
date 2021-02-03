5c92dc6824e714ccf7f32b9f26e2390574e33bac
Shreya Pandey <shreya.pandey.emip@gmail.com>
2021-02-03 13:58:05 +0530

    hackerrank compare the triplets solution python

---
+++ zipandbool.py
@@ -0,0 +1,43 @@
+
+#!/bin/python3
+
+'''Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Comparison points is the total points a person earned.
+
+Given a and b, determine their respective comparison points.'''
+
+
+import math
+import os
+import random
+import re
+import sys
+
+def compareTriplets(a, b):
+    score=[0,0]
+    for i,j in zip(a,b):
+        score[0]+=i>j
+        score[1]+=j>i
+    return score
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
