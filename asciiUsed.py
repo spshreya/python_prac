f06f096e75582cf56726312db27e9359729db655
Shreya Pandey <shreya.pandey.emip@gmail.com>
2021-12-14 19:42:44 +0530

    Hackerrank Designer PDF Viewer in Python

---
+++ asciiUsed.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    maxx = 0
+    for i in range(len(word)):
+        if h[ord(word[i])-97]>maxx:
+            maxx = h[ord(word[i])-97]
+    return len(word)*maxx
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
